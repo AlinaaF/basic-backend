a1a41b0755587426e2c6a5f8fe129ebe1f62f097
Ilia Kisliakovskii <kisliakovskiii@yandex.ru>
2021-04-08 17:32:48 +0300

    Update README.md

--- script/db_init.py
+++ script/db_init.py
@@ -7,5 +7,6 @@ with connectionPool.get_resource() as connection:
     cursor.execute("INSERT INTO users VALUES ('Ivan', 'Ivanov', '+79111111111')")
     cursor.execute("INSERT INTO users VALUES ('John', 'Doe', '+449111111111')")
     cursor.execute("INSERT INTO users VALUES ('Johann', 'Schmidt', '+499111111111')")
+    cursor.execute("INSERT INTO users VALUES ('Alina', 'Fayzrakhmanova', '+79992121155')")
     connection.commit()
     cursor.close()
